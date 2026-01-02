7f6353def7820492b717b523ca9f762b81abd38d
himanshu Ghorai <himanshughorai000@gmail.com>
2026-01-02 21:07:38 +0530

    Add basic pattern programs

--- javascript-practice/day2/project/loops.js
+++ javascript-practice/day2/project/loops.js
@@ -30,27 +30,77 @@
 
 // Reverse a number using loop
 
-let num = 12345;
-// console.log(num.reverse());
+// let num = 12345;
+// // console.log(num.reverse());
 
-let Reverse = 0;
+// let Reverse = 0;
 
-while (num > 0) {
-    let digit = num % 10;
-    Reverse = Reverse * 10 + digit;
-    num = Math.floor(num / 10);
+// while (num > 0) {
+//     let digit = num % 10;
+//     Reverse = Reverse * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+// console.log(Reverse);
+
+// // Print multiplication table of a given number
+
+
+// let TableNumber = 3;
+
+// let multiplicationNumber = 55;
+
+// for (let i = 1; i <= multiplicationNumber; i++) {
+//     console.log(`${i} X ${TableNumber} = ${i * TableNumber} `);
+
+// }
+
+
+
+// Nested Loops
+
+// Print pattern:
+// Square Pattern
+
+// let str = "*";
+
+// for (let i = 0; i < 4; i++) {
+//     let row = "";
+//     for (let j = 0; j < 4; j++) {
+//         row += str;
+//     }
+//     console.log(row);
+// }
+
+
+// Right Triangle
+
+// let str = "*";
+// let count = "";
+
+// for (let i = 0; i < 5; i++) {
+//     count += str;
+//     console.log(count);
+// }
+
+// Number Triangle
+
+for (let i = 1; i < 6; i++) {
+    let row = '';
+    for (let n = 1; n <= i; n++) {
+        row += n;
+    }
+    console.log(row);
+
 }
-console.log(Reverse);
-
-// Print multiplication table of a given number
 
 
-let TableNumber = 3;
+// *
+// **
+// ***
+// ****
 
-let multiplicationNumber = 55;
-
-for (let i = 1; i <= multiplicationNumber; i++) {
-    console.log(`${i} X ${TableNumber} = ${i * TableNumber} `);
-
-}
+// 1
+// 12
+// 123
+// 1234
 
