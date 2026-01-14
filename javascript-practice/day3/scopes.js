93aca948ef54f44efad848af093f055255a93148
himanshu Ghorai <himanshughorai000@gmail.com>
2026-01-14 22:54:39 +0530

    scope for today

---
+++ javascript-practice/day3/scopes.js
@@ -0,0 +1,20 @@
+let a = 12;
+const b = 134;
+var c = 345;
+
+
+console.log(a, b, c);
+
+
+function one() {
+    const username = "himanshu";
+    function two() {
+        const website = "youtube";
+        console.log(username);
+
+    }
+    // console.log(website);
+
+    two();
+}
+one();
